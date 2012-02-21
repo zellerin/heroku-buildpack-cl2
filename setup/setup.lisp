d89474c1a62e969944474b7b8f8e6d9feea479b5
Mike Travers <mt@alum.mit.edu>
2012-02-20 17:56:37 -0800

    tweak tweak

--- setup/setup.lisp
+++ setup/setup.lisp
@@ -8,16 +8,21 @@
 
 (defvar *app-home* (butlast (pathname-directory *load-pathname*)))
 
+(require :asdf)
+
+(load (make-pathname :directory (append *app-home* '("repos" "portableaserve" "aserve"))
+		     :defaults "aserve.asd"))
+
 (if (probe-file (make-pathname :directory (append *app-home* '("quicklisp")) :defaults "setup.lisp"))
     (load (make-pathname :directory (append *app-home* '("quicklisp")) :defaults "setup.lisp"))
     (progn
       (load (make-pathname :directory (append *app-home* '("lib")) :defaults "quicklisp.lisp"))
 ;      (quicklisp-quickstart:install :path (make-pathname :directory (append *app-home* '("quicklisp"))))
       (funcall (symbol-function (find-symbol "INSTALL" (find-package "QUICKLISP-QUICKSTART")))
-	       (make-pathname :directory (append *app-home* '("quicklisp"))))	       
+	       :path (make-pathname :directory (append *app-home* '("quicklisp"))))	       
       ))
 
-(require :asdf)
+
 (load (make-pathname :directory *app-home* :name *app-name* :type "asd"))
 
 
