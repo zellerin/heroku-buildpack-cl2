0469d7fa7cb746cf42d4df0f506a02e9eb522c67
Mike Travers <mt@alum.mit.edu>
2012-02-21 19:50:08 -0800

    trying to get compile to work

--- setup/compile.lisp
+++ setup/compile.lisp
@@ -1,5 +1,9 @@
 (in-package :cl-user)
 
+(defvar *cache-dir* (pathname-directory (pathname (concatenate 'string (getenv "CACHE_DIR") "/"))))
+
 (load (make-pathname :defaults *load-pathname* :name "setup"))
 
 (ql:quickload *app-name*)
+
+(ccl:quit)
