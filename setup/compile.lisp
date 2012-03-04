65ae5610582f7394f70e64085c62f580dcfe66c7
Mike Travers <mt@alum.mit.edu>
2012-03-04 12:13:57 -0800

    change tack and save the whole app, much simpler.  in progress

--- setup/compile.lisp
+++ setup/compile.lisp
@@ -1,9 +1,33 @@
 (in-package :cl-user)
 
-(defvar *cache-dir* (pathname-directory (pathname (concatenate 'string (getenv "CACHE_DIR") "/"))))
+;;; Customize
 
-(load (make-pathname :defaults *load-pathname* :name "setup"))
+(defvar *app-name* "example")
+
+;;; Standard
+
+(defvar *app-dir* (butlast (pathname-directory *load-pathname*)))
+
+(require :asdf)
+(load (make-pathname :directory (append *app-dir* '("lib")) :defaults "quicklisp.lisp"))
+
+;;; Not needed?
+;(defvar *cache-dir* (pathname-directory (pathname (concatenate 'string (getenv "CACHE_DIR") "/"))))
+
+;(load (make-pathname :defaults *load-pathname* :name "setup"))
+
+(asdf:clear-system "acl-compat")
+
+(load (make-pathname :directory (append *app-dir* '("repos" "portableaserve" "acl-compat"))
+		     :defaults "acl-compat.asd"))
+(load (make-pathname :directory (append *app-dir* '("repos" "portableaserve" "aserve"))
+		     :defaults "aserve.asd"))
+
+(load (make-pathname :directory *app-dir* :name *app-name* :type "asd"))
 
 (ql:quickload *app-name*)
 
-(ccl:quit)
+(save-application
+ (make-pathname :directory (getenv "BUILD_DIR") :name *app-name* :type "app")
+; :toplevel-function
+ )
