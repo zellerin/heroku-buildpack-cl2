c6e5cff4d668b8d3f3b187a0eca963e10a4b9a82
Anton Vodonosov <avodonosov@yandex.ru>
2013-02-04 01:35:12 +0300

    work

--- bin/compile.lisp
+++ bin/compile.lisp
@@ -7,15 +7,6 @@
 (defvar *cache-dir* (pathname-directory (pathname (concatenate 'string (asdf::getenv "CACHE_DIR") "/"))))
 (defvar *buildpack-dir* (pathname-directory (pathname (concatenate 'string (asdf::getenv "BUILDPACK_DIR") "/"))))
 
-(defparameter *fasl-dir* (merge-pathnames "fasl/" *build-dir2*))
-(format t "*build-dir*: ~A~%" *build-dir*)
-(format t "*fasl-dir*: ~A~%" *fasl-dir*)
-
-(format t "XDG_CACHE_HOME: ~A~%" (asdf:getenv "XDG_CACHE_HOME"))
-
-(load (make-pathname :directory *build-dir* :defaults "buildpack-utils.lisp"))
-(add-asdf-output-translation *build-dir2* *fasl-dir*)
-
 (let ((ql-setup (merge-pathnames "quicklisp/setup.lisp" *build-dir2*)))
   (format t "ql-setup: ~A~%" ql-setup)
   (if (probe-file ql-setup)
@@ -24,6 +15,12 @@
 	(load (make-pathname :directory (append *buildpack-dir* '("lib")) :defaults "quicklisp.lisp"))
 	(funcall (read-from-string "quicklisp-quickstart:install")
 		 :path (make-pathname :directory (pathname-directory ql-setup))))))
+
+(defparameter *fasl-dir* (merge-pathnames "fasl/" *build-dir2*))
+(format t "*build-dir*: ~A~%" *build-dir*)
+(format t "*fasl-dir*: ~A~%" *fasl-dir*)
+(load (make-pathname :directory *build-dir* :defaults "buildpack-utils.lisp"))
+(add-asdf-output-translation *build-dir2* *fasl-dir*)
 
 ;;; Load the application from sources
 (load (make-pathname :directory *build-dir* :defaults "heroku-setup.lisp"))
