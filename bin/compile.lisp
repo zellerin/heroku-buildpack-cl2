2dc26d9a9dead676a2dd094e1d24a0eae9c9bc8c
Anton Vodonosov <avodonosov@yandex.ru>
2013-02-23 07:04:59 +0300

    Add :VERSION parameter to REQUIRE-QUICKLISP

--- bin/compile.lisp
+++ bin/compile.lisp
@@ -6,14 +6,23 @@
 (defvar *cache-dir* (pathname (concatenate 'string (asdf::getenv "CACHE_DIR") "/")))
 (defvar *buildpack-dir* (pathname (concatenate 'string (asdf::getenv "BUILDPACK_DIR") "/")))
 
-(defun require-quicklisp ()
+(defmacro fncall (funname &rest args)
+  `(funcall (read-from-string ,funname) ,@args))
+
+(defun require-quicklisp (&key version)
+  "VERSION if specified must be in format YYYY-MM-DD"
   (let ((ql-setup (merge-pathnames "quicklisp/setup.lisp" *build-dir*)))
     (if (probe-file ql-setup)
         (load ql-setup)
         (progn
           (load (merge-pathnames "bin/quicklisp.lisp" *buildpack-dir*))
-          (funcall (read-from-string "quicklisp-quickstart:install")
-                   :path (make-pathname :directory (pathname-directory ql-setup)))))))
+          (fncall "quicklisp-quickstart:install"
+                   :path (make-pathname :directory (pathname-directory ql-setup)))))
+    (when version
+      (fncall "ql-dist:install-dist"
+              (format nil "http://beta.quicklisp.org/dist/quicklisp/~A/distinfo.txt"
+                      version)
+              :replace t :prompt nil))))
 
 ;;; stacktrace printing, copy/pasted from the ql-test by Fare:
 ;;; ssh://common-lisp.net/home/frideau/git/ql-test.git
